2d92e2d7462b9b3de5dff01b8c0a07198a9e6ac1
Petka Antonov <petka_antonov@hotmail.com>
2011-09-04 16:08:29 +0300

    typeOf any htmlelement returns "node", added method to make contracts, argument signatures without "..." are strict in the amount of arguments received, signatures can now have whitespace in them

--- argumentshook.js
+++ argumentshook.js
@@ -8,10 +8,43 @@
 ( function( global ){
 
 var legitToString = /^\[object\s[a-zA-Z\$_-][a-zA-Z0-9\$_-]*?\]$/,
+	element = /^html[A-Za-z0-9]*?element$/,
+	whiteSpace = /\s\s*/g,
 	arrayProtoMethods = "concat join pop push reverse shift slice sort splice unshift".split(" "),
+	
 	i,
 	l = arrayProtoMethods.length, 
 	prMethod;
+	
+	
+	//Automatically ensure a function is called with certain argument signature and returns 
+	function ensure( signature ) {
+		if( this.isHooked ) {
+		return this;
+		}
+	
+	var self = this, returnType = "undefined", separated;
+	signature = signature.replace( whiteSpace, "" ).toLowerCase();
+	separated = signature.split( "->" );
+	returnType = separated.length > 1 ? separated[1] : "undefined";
+	signature = separated[0];
+		
+		function ensured() {
+		var args = new Arguments( arguments ), r, rType;
+		args.ensure( signature );
+		this.arguments = args;
+		r = self.apply( this, args.argumentList );
+		rType = typeOf( r );
+			if( rType != "any" && rType != returnType ) {
+			throw new TypeError(  "Expected <"+returnType+">, got "+r+"<"+rType+"> instead.\n" );
+			}
+			else {
+			return r;
+			}
+		}
+	ensured.isHooked = true;
+	return ensured;
+	}
 	
 	
 	//Overload function with a key value pair object where
@@ -107,13 +140,13 @@ var legitToString = /^\[object\s[a-zA-Z\$_-][a-zA-Z0-9\$_-]*?\]$/,
 			}
 			
 		// turn "[object Example]" into "example"
-		spacePos = objStr.lastIndexOf( " " ); 
-		return objStr.substr( spacePos + 1, objStr.lastIndexOf( "]" ) - spacePos - 1 ).toLowerCase();
+		spacePos = objStr.lastIndexOf( " " );                                                                //htmlxxxelement => node
+		return objStr.substr( spacePos + 1, objStr.lastIndexOf( "]" ) - spacePos - 1 ).toLowerCase().replace( element, "node" ); 
 		break;	
 		}
 	
 	}
-		
+	
 	function getContinuity( str ) {
 	var type = str.replace( "...", "" );
 	
@@ -140,6 +173,7 @@ var legitToString = /^\[object\s[a-zA-Z\$_-][a-zA-Z0-9\$_-]*?\]$/,
 	this.argumentList = argsArray;
 	this.original = args;
 	this.thisFunction = args && args.callee;
+	this.invalidValue = "";
 	this.invalidArg = "";
 	this.expectedN = -1;
 	this.expected = "";
@@ -154,8 +188,8 @@ var legitToString = /^\[object\s[a-zA-Z\$_-][a-zA-Z0-9\$_-]*?\]$/,
 		constructor: Arguments,
 				
 		match: function( signature ){
-		var validatorArgs = signature.toLowerCase().split(","),
-			i, l = validatorArgs.length, validArg, typeList = this.typeList;
+		var validatorArgs = signature.replace( whiteSpace, "" ).toLowerCase().split(","),
+			i, l = validatorArgs.length, validArg, typeList = this.typeList, isEqualLength;
 
 			if( !l ) {
 			return true;
@@ -166,16 +200,25 @@ var legitToString = /^\[object\s[a-zA-Z\$_-][a-zA-Z0-9\$_-]*?\]$/,
 
 				if( !( i in typeList ) || typeList[i] != validArg ) {
 				this.expectedN = i;
+				this.invalidValue = this[i];
 				this.expected = validArg;
 				this.invalidArg = typeList[i] || "undefined";
 				return false;
 				}
 			}
-
+		
+		isEqualLength = l === this.length;
 		validArg = getContinuity( validatorArgs[i] );
 			if( validArg.type && !validArg.isContinuous ) {
-				if( !( ( i in typeList ) && validArg.type == typeList[i] ) ) {
+				if( !( ( i in typeList ) && validArg.type == typeList[i] && isEqualLength ) ) {
+				
+					if( !isEqualLength ) {
+					i++;
+					validArg.type = "undefined";
+					}
+				
 				this.expectedN = i;
+				this.invalidValue = this[i];
 				this.expected = validArg.type;
 				this.invalidArg = typeList[i] || "undefined";
 				return false;
@@ -191,6 +234,7 @@ var legitToString = /^\[object\s[a-zA-Z\$_-][a-zA-Z0-9\$_-]*?\]$/,
 				for( ; i < l; ++i ) {	
 					if( typeList[i] != validArg.type ) {
 					this.expectedN = i;
+					this.invalidValue = this[i];
 					this.expected = validArg.type;
 					this.invalidArg = typeList[i];
 					return false;
@@ -223,7 +267,7 @@ var legitToString = /^\[object\s[a-zA-Z\$_-][a-zA-Z0-9\$_-]*?\]$/,
 		
 		throwError: function( msg ){
 		var message = typeof msg != "string" ? "" : msg;
-		throw new TypeError(  "Expected "+this.expected+" as "+(this.expectedN+1)+". argument, got "+this.invalidArg +" instead.\n"+ message );
+		throw new TypeError(  "Expected <"+this.expected+"> as "+(this.expectedN+1)+". argument, got <"+this.invalidArg+">"+this.invalidValue +" instead.\n"+ message );
 		}
 	};
 		
@@ -231,7 +275,8 @@ var legitToString = /^\[object\s[a-zA-Z\$_-][a-zA-Z0-9\$_-]*?\]$/,
 	prMethod = arrayProtoMethods[i];
 	Arguments.prototype[prMethod] = Array.prototype[prMethod];
 	}
-		
+
+global.Function.prototype.ensure = ensure;	
 global.Function.prototype.argumentsHook = argumentsHook;
 global.Function.prototype.overload = overload;
 global.Function.prototype.hookMethods = hookMethods;
